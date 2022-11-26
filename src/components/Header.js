78fc2245b8b622012bb45dde59c783029d22f261
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 11:07:57 -0500

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,61 @@ import React from "react";
 import HomeButton from "./HomeButton";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import NavButton from "./NavButton";
 
 function Header() {
-return (
+//   const buttonStyle = {
+//     backgroundColor: "green",
+//     border: "none",
+//     color: "white",
+//     padding: "15px 32px",
+//     textAlign: "center",
+//     textDecoration: "none",
+//     display: "inline-block",
+//     fontSize: "16px",
+//   };
+  return (
     <div>
-    <div>
+      <div>
         <HomeButton />
       </div>
+      {/* <NavLink>
+        <button style={buttonStyle}>
+          <p>Home</p>
+        </button>
+      </NavLink> */}
+      
       <nav>
-      <NavLink
-className={({ isActive }) =>
-isActive ? "link-active" : "link-inactive" }
-to="/" > Home
-</NavLink>
-<NavLink
-className={({ isActive }) =>
-isActive ? "link-active" : "link-inactive" }
-to="/about" > About
-</NavLink>
-<NavLink
-className={({ isActive }) =>
-isActive ? "link-active" : "link-inactive" }
-to="/contact" > Contact
-</NavLink>
-</nav>
-</div>
-);
+        {/* <NavLink
+          className={({ isActive }) =>
+            isActive ? "link-active" : "link-inactive"
+          }
+          to="/"
+        >
+          {" "}
+          Home
+        </NavLink> */}
+        <NavButton to="/" label="Home" />
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "link-active" : "link-inactive"
+          }
+          to="/about"
+        >
+          {" "}
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "link-active" : "link-inactive"
+          }
+          to="/contact"
+        >
+          {" "}
+          Contact
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 export default Header;
