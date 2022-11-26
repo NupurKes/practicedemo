9e56d29776f6ebdfcdbcb10c2408547f993e525a
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 08:10:28 -0500

    homebutton added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeButton from "./HomeButton";
 function Header() {
 return (
+    <div>
+    <div>
+<HomeButton />
+</div>
 <div>
 <Link to="/">Home</Link>
 <Link to="/about">About</Link>
 <Link to="/contact">Contact</Link>
+</div>
 </div>
 );
 }
