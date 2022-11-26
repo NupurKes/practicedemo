84c31941b13b5af07cae497b868e28d4cb968b0d
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 11:28:11 -0500

    Adding style to Buttons

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -1,35 +1,36 @@
 import { NavLink } from "react-router-dom";
 import { useResolvedPath } from "react-router-dom";
 import { useMatch } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function NavButton(props) {
   let resolved = useResolvedPath(props.to);
   let match = useMatch({ path: resolved.pathname, end: true });
   const buttonStyle = {
-    backgroundColor: "green",
+    //backgroundColor: "green",
     border: "none",
     color: "white",
-    padding: "15px 32px",
+    padding: "10px 10px",
     textAlign: "center",
     textDecoration: "none",
     display: "inline-block",
-    fontSize: "16px",
+    fontSize: "10px",
   };
   const activeButtonStyle = {
-    backgroundColor: "red",
+    backgroundColor: "orange",
     border: "none",
     color: "black",
-    padding: "15px 32px",
+    padding: "12px 12px",
     textAlign: "center",
     textDecoration: "none",
     display: "inline-block",
-    fontSize: "16px",
+    fontSize: "11px",
   };
   return (
     <NavLink to={props.to}>
-      <button style={match ? activeButtonStyle : buttonStyle}>
+      <Button  variant="contained" style={match ? activeButtonStyle : buttonStyle}>
         <p>{props.label}</p>
-      </button>
+      </Button>
     </NavLink>
   );
 }
