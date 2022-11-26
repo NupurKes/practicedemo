71c1826d29043ed2475ea0c15c3e5f5a37ce16be
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 14:05:48 -0500

    update the  pages

---
+++ src/pages/Hobbies.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hobbies() {
+  return (
+    <div className = "content">
+        <h1>
+            My Hobbies
+        </h1>
+        <h3>I love traveling, cooking, games, working out and photography! Oh, and by the way, who doesn't love friends?</h3>
+    </div>
+  )
+}
