a2fff8fe019a6e3a859f2902bf8b9169a91aa253
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 19:37:42 -0500

    education page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className = "content">
-    <h1>Welcome To my WebPage</h1>
+    <h1>Welcome To My WebPage</h1>
     
     </div>
   )
