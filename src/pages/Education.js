e56b5176e4b8cee1a346fc5b153eea8adae204f4
NupurKes <104525328+NupurKes@users.noreply.github.com>
2022-11-26 19:37:12 -0500

    Final update

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -4,11 +4,10 @@ export default function Education() {
   return (
     <div className = "content">
         <h1>My Education</h1>
-        <ul>
-             <li>MASTER DEGREE IN SOFTWARE ENGINEERING</li>
-             <li>BACHELOR DEGREE IN SOFTWARE ENGINEERING</li>
-             <li>HIGH SCHOOL DIPLOMA IN MATHEMATICS AND PHYSICS </li>
-        </ul>
+        <p>MASTER DEGREE IN SOFTWARE ENGINEERING <br/>
+             BACHELOR DEGREE IN SOFTWARE ENGINEERING<br/>
+             HIGH SCHOOL DIPLOMA IN MATHEMATICS AND PHYSICS 
+        </p>
   
         </div>
   )
